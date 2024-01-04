f1476dd9d9f5d85f7596dceea8570ef35ab4ecac
Aadarsha Khatri <khatriaadarsh2@gmail.com>
2024-01-04 13:28:37 +0545

    Exam files

--- src/Main.java
+++ src/Main.java
@@ -9,9 +9,9 @@ public class Main {
 
         // Tshirt class ma naya 3 object haru define garera order nam deko
 
-        Tshirt order1 = new Tshirt("Gorkhali Batman","123",1200,"thulo","welcome to my shop",new String[]{"XL","L","M"});
-        Tshirt order2 = new Tshirt("Superman","123",1300,"kappa","welcome to my shop",new String[]{"XL","L","M"});
-        Tshirt order3 = new Tshirt("Caption america","123",1400,"lew","welcome to my shop",new String[]{"XL","L","M"});
+        Tshirt order1 = new Tshirt("Gorkhali Batman","123",1200,"thulo","You either die hero or live long enough to see yourself as a villan",new String[]{"XL","L","M"});
+        Tshirt order2 = new Tshirt("Nepal Classic","123",1300,"kappa","I love Nepal",new String[]{"XL","L","M"});
+        Tshirt order3 = new Tshirt("Gautam Buddha","123",1400,"lew","Nothing is permanent",new String[]{"XL","L","M"});
 // order bhanne class m a naya 3 object define garera customer haru lekheko
         Order customer1=new Order("Aadrsha",123423424,new int[]{12,33,42},new int[]{1200,1300,1000});
         Order customer2=new Order("Ram",123423423,new int[]{10,30,43},new int[]{1200,1350,1800});
